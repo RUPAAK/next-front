8ad28116e7b31b864ccb989a0768019af99d6b45
rupaak <rupakt525@gmail.com>
2022-07-10 11:27:27 +0545

    changes

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -12,37 +12,38 @@ export type ArticleProps = {
 };
 
 const Article = ({ total, blogs }: ArticleProps) => {
-  return (
-    <Box maxWidth="xs">
-      <Typography variant="h4">2022</Typography>
-      <Divider />
-      <Typography>In Progress...</Typography>
-      {/* {blogs.length > 0 &&
-        blogs.map((blog, index) => (
-          <DateArticle key={index} date="2020" blogs={blogs} />
-        ))} */}
-    </Box>
-  );
+  return <h1>Hello</h1>
+  // return (
+  //   <Box maxWidth="xs">
+  //     <Typography variant="h4">2022</Typography>
+  //     <Divider />
+  //     <Typography>In Progress...</Typography>
+  //     {/* {blogs.length > 0 &&
+  //       blogs.map((blog, index) => (
+  //         <DateArticle key={index} date="2020" blogs={blogs} />
+  //       ))} */}
+  //   </Box>
+  // );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const blogs = await adminSerice.getAllArticles();
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const blogs = await adminSerice.getAllArticles();
 
-  if (!blogs) {
-    console.log("hit");
-    return {
-      props: {
-        total: 0,
-        blogs: [],
-      },
-    };
-  }
-  return {
-    props: {
-      total: blogs.total,
-      blogs: blogs.data,
-    },
-  };
-};
+//   if (!blogs) {
+//     console.log("hit");
+//     return {
+//       props: {
+//         total: 0,
+//         blogs: [],
+//       },
+//     };
+//   }
+//   return {
+//     props: {
+//       total: blogs.total,
+//       blogs: blogs.data,
+//     },
+//   };
+// };
 
 export default Article;
