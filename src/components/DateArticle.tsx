e5f6e7870dbeb17d613a8cd1bb92f188dde6a8c9
rupaak <rupakt525@gmail.com>
2022-08-01 09:11:21 +0545

    single article strapi

--- src/components/DateArticle.tsx
+++ src/components/DateArticle.tsx
@@ -1,21 +1,21 @@
 import { Box, Divider, Typography } from "@mui/material";
 import Article from "page-components/home/HomeArticle";
 import React, { FC } from "react";
-import { Blog } from "types/blog";
+import { Blog, StrapiArticle } from "types/blog";
 import EachArticle from "./EachArticle";
 
 type DateArticleProps = {
   date: string;
-  blogs: Blog[];
+  articles: StrapiArticle[];
 };
 
-const DateArticle: FC<DateArticleProps> = ({ date, blogs }) => {
+const DateArticle: FC<DateArticleProps> = ({ date, articles }) => {
   return (
     <Box maxWidth="xs">
    
 
-      {blogs.map((blog, index) => (
-        <EachArticle blog={blog} key={index} />
+      {articles.map((article, index) => (
+        <EachArticle article={article} key={index} />
       ))}
     </Box>
   );
