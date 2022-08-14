7e560bdd23c5c13b447a5815805906429d92e7f0
rupaak <rupakt525@gmail.com>
2022-08-14 09:02:42 +0545

    some changes

--- src/aboutme.ts
+++ src/aboutme.ts
@@ -6,12 +6,10 @@ export const aboutMe = (): string => {
       <p>&nbsp;</p>
       
       <p><span style="font-size:16px">Hey, I am Rupak. I am a software developer at BitpointX. This is my portfolio project that have been inspired from <strong>Tania Rascia.&nbsp;</strong>I have build all the components need for this project from backend to frontend. I love learning new tech and I am bad on every one of them.&nbsp;&nbsp;</span></p>
-      
-      <p><span style="font-size:16px">The idea of this site goes completely to Tania. Hope you like it.</span></p>
-      
+            
       <p>&nbsp;</p>
       
-      <p><span style="font-size:16px">Any collabration or project, you can connect me here.</span></p>
+      <p><span style="font-size:16px">You can connect me here.</span></p>
       
       <ul>
           <li style="text-align:justify"><a href="https://www.linkedin.com/in/rupaak"><strong>Linkedin</strong></a></li>
@@ -20,18 +18,11 @@ export const aboutMe = (): string => {
       
       <p>&nbsp;</p>
       
-      <p><span style="font-size:22px">What I can do</span></p>
-      
-      <p><em><span style="font-size:16px">I can do JS stack specially MERN, mongodb and postgres, flutter and git. There is something called TDD and Typescript that I know and....</span></em></p>
-      
       <p>&nbsp;</p>
       
-      <p><span style="font-size:22px">Fact for Future</span></p>
+      <p><span style="font-size:22px">Info</span></p>
       
       <ul>
-          <li><span style="font-size:16px">I am trying to be more active on open source.</span></li>
-          <li><span style="font-size:16px">Eager to start freelancing.</span></li>
-          <li><span style="font-size:16px">At this time of writing (May 14 ,2022) I am learning flutter and DSA.</span></li>
           <li><span style="font-size:16px">I read and try to write</span><span style="font-size:16px">&nbsp;</span></li>
       </ul>
       
